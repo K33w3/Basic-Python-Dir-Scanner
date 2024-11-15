64120a344eed84ae16d3de57abea30cc5a083b8c
KafetzisThomas <tomkafetzis06@gmail.com>
2024-11-15 23:26:37 +0200

    Split code into smaller functions

--- fuzzerweb_better.py
+++ fuzzerweb_better.py
@@ -1,11 +1,44 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
 import sys
 import time
 import requests
-import argparse
+
+BOLD = "\033[1m"
+END = "\033[0m"
 
 
-def main(argv):
+def scan_urls(wordlist, url):
+    with open(wordlist, "r") as file:
+        content = file.read().splitlines()
+
+        for line in content:
+            start = time.time()
+            x = f"{url}/{line}"
+            y = requests.get(x)
+            end = time.time()
+
+            if y.status_code != 404:
+                status_message = f"{BOLD}{x} [ Status: {y.status_code} | Size: {len(y.text)} | Duration: {end - start:.2f} s ]{END}"
+            else:
+                status_message = f"{x} [ Status: {y.status_code} | Size: {len(y.text)} | Duration: {end - start:.2f} s ]"
+
+            print(status_message)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: fuzzerweb_better.py <wordlist> <URL>")
+        sys.exit()
+
+    try:
+        wordlist = sys.argv[1]
+        url = sys.argv[2].rstrip("/")
+    except IndexError:
+        print("Please ensure all arguments are correctly specified.")
+        sys.exit()
+
     print(
         """
  _______  __   __  _______  _______  _______  ______   
@@ -18,61 +51,12 @@ def main(argv):
         """
     )
 
-    print(f"Wordlist: {args.wordlist}")
-    print(f"Url: {args.url}")
-    print("---------------------------------------")
+    print(f"Wordlist: {wordlist}")
+    print(f"Url: {url}")
+    print("------" * 4)
 
-    with open(file=args.wordlist, mode="r") as file:
-        f1 = file.read().splitlines()
-        for line in f1:
-            start = time.time()
-            x = f"{args.url}/{line}"
-            y = requests.get(x)
-            end = time.time()
-
-            if y.status_code != 404 or args.verb:
-                if y.status_code != 404:
-                    status_message = f"{style.BOLD}{x}\t[ Status: {y.status_code} | Size: {len(y.text)} | Duration: {end - start:.2f} s ]{style.END}"
-                else:
-                    status_message = f"{x}\t[ Status: {y.status_code} | Size: {len(y.text)} | Duration: {end - start:.2f} s ]"
-
-                print(status_message)
+    scan_urls(wordlist, url)
 
 
 if __name__ == "__main__":
-
-    class style:
-        BOLD = "\033[1m"
-        END = "\033[0m"
-        Yellow = ""
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-w",
-        "--wordlist",
-        default="0",
-        dest="wordlist",
-        help="Provide wordlist for fuzzing",
-        required=True,
-        type=str,
-    )
-    parser.add_argument(
-        "-u",
-        "--url",
-        default="0",
-        dest="url",
-        help="Provide url to fuzz",
-        required=True,
-        type=str,
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        default="false",
-        dest="verb",
-        help="Give this argument if you want it to be a verbose output",
-        action="store_true",
-    )
-    args = parser.parse_args()
-
-    main(sys.argv[1:])
+    main()
