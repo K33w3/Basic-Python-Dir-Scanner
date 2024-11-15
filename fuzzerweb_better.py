431759aea8d06f5db2bc0fde908415f1dec4eb84
KafetzisThomas <tomkafetzis06@gmail.com>
2024-11-15 22:50:46 +0200

    Update file handling & url request processing

--- fuzzerweb_better.py
+++ fuzzerweb_better.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-import requests,time
-import sys, argparse
+import sys
+import time
+import requests
+import argparse
 
 
 def main(argv):
-	print("""
+    print(
+        """
  _______  __   __  _______  _______  _______  ______   
 |  _    ||  | |  ||       ||       ||       ||    _ |  
 | |_|   ||  | |  ||____   ||____   ||    ___||   | ||  
@@ -12,64 +15,64 @@ def main(argv):
 |  _   | |       || ______|| ______||    ___||    __  |
 | |_|   ||       || |_____ | |_____ |   |___ |   |  | |
 |_______||_______||_______||_______||_______||___|  |_|
-		""")
+        """
+    )
 
-	print("wordlist: ",args.wordlist)
-	print("url: ",args.url)
-	print("---------------------------------------")
+    print(f"Wordlist: {args.wordlist}")
+    print(f"Url: {args.url}")
+    print("---------------------------------------")
 
-	f = open(args.wordlist, 'r')
-	f1 = f.read().splitlines()
-	for line in f1:
-		start = time.time()
-		x = args.url.replace("BUZZ", line)
-		y = requests.get(x)
+    with open(file=args.wordlist, mode="r") as file:
+        f1 = file.read().splitlines()
+        for line in f1:
+            start = time.time()
+            x = f"{args.url}/{line}"
+            y = requests.get(x)
+            end = time.time()
 
-		if(args.verb == True):
-			if(y.status_code != 404):
-				end = time.time()
-				print(style.BOLD + x,"[", "Status: ",y.status_code, "Size: ", len(y.text), "Duration: ", end-start,"s","]"+style.END)
-			else:
-				end = time.time()
-				print(x,"[", "Status: ",y.status_code, "Size: ", len(y.text), "Duration: ", end-start,"s","]")
+            if y.status_code != 404 or args.verb:
+                if y.status_code != 404:
+                    status_message = f"{style.BOLD}{x}\t[ Status: {y.status_code} | Size: {len(y.text)} | Duration: {end - start:.2f} s ]{style.END}"
+                else:
+                    status_message = f"{x}\t[ Status: {y.status_code} | Size: {len(y.text)} | Duration: {end - start:.2f} s ]"
 
-		elif(y.status_code != 404):
-			end = time.time()
-			print(style.BOLD + x,"[", "Status: ",y.status_code, "Size: ", len(y.text), "Duration: ", end-start,"s","]"+style.END)
+                print(status_message)
 
 
 if __name__ == "__main__":
-	
-	class style:
-		BOLD = '\033[1m'
-		END = '\033[0m'
-		Yellow = ''
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-w', '--wordlist',
-						default='0',
-						dest='wordlist',
-						help='Provide wordlist for fuzzing',
-						required = True,
-						type=str
-						)
-	parser.add_argument('-u', '--url',
-						default='0',
-						dest='url',
-						help='Provide url to fuzz',
-						required=True,
-						type=str)
-	parser.add_argument('-v', '--verbose',
-						default='false',
-						dest='verb',
-						help='Give this argument if you want it to be a verbose output',
-						action='store_true')
-	#parser.add_argument('-b', '--buzz',
-	#					default='0',
-	#					dest='buzz',
-	#					help='Provide url with keyword BUZZ to fuzz location',
-	#					type=str)
+    class style:
+        BOLD = "\033[1m"
+        END = "\033[0m"
+        Yellow = ""
 
-	args = parser.parse_args()
-	
-	main(sys.argv[1:])
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-w",
+        "--wordlist",
+        default="0",
+        dest="wordlist",
+        help="Provide wordlist for fuzzing",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "-u",
+        "--url",
+        default="0",
+        dest="url",
+        help="Provide url to fuzz",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default="false",
+        dest="verb",
+        help="Give this argument if you want it to be a verbose output",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    main(sys.argv[1:])
